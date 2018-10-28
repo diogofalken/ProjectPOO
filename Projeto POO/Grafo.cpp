5c130beaed9772bb005437166ed74965c2e58f08
diogofalken <diogofalken0@gmail.com>
2018-10-28 01:49:59 +0100

    Added comments to all the functions in Classo Grafo

--- Projeto POO/Grafo.cpp
+++ Projeto POO/Grafo.cpp
@@ -10,78 +10,242 @@ Grafo::~Grafo() {
 
 }
 
+//-------------------------------------------------------------------
+//Método: Load
+//Parametros:
+// Entrada:
+//     fich_grafo: Ficheiro dos dados do Grafo
+//     fich_pessoas: Ficheiro das pessoas que querem fazer as viagens entre as fronteiras
+// Retorno:
+//    true/false, mediante a leitura correcta!
+//-------------------------------------------------------------------
 bool Grafo::Load(const string &fich_grafo, const string &fich_pessoas) {
-
+	return NULL;
 }
 
+//-------------------------------------------------------------------
+//Método: ContarNos
+//Parametros:
+// Entrada:
+//
+// Retorno:
+//    Numero de nos do grafo
+//-------------------------------------------------------------------
 int Grafo::ContarNos() {
-
+	return NULL;
 }
 
+//-------------------------------------------------------------------
+//Método: ContarArcos
+//Parametros:
+// Entrada:
+//
+// Retorno:
+//    Numero de arcos/arestas do grafo
+//-------------------------------------------------------------------
 int Grafo::ContarArcos() {
 	return -1;
 }
 
+//-------------------------------------------------------------------
+//Método: Memoria
+//Parametros:
+// Entrada:
+//
+// Retorno:
+//    Quanta memoria está a ser ocupada pelo grafo
+//-------------------------------------------------------------------
 int Grafo::Memoria() {
 	return -1;
 }
 
+//-------------------------------------------------------------------
+//Método: NoMaisArcos
+//Parametros:
+// Entrada:
+//
+// Retorno:
+//    No/fronteira com mais arcos ( se for mais que um da return numa lista)
+//-------------------------------------------------------------------
 list<int> *Grafo::NoMaisArcos() {
 	return NULL;
 }
 
+//-------------------------------------------------------------------
+//Método: Adjancencia
+//Parametros:
+// Entrada:
+//     v1: vertice 1
+//     v2: vertice 2
+// Retorno:
+//    true/false, mediante serem adjacentes ou nao
+//-------------------------------------------------------------------
 bool Grafo::Adjacencia(int v1, int v2) {
 	return false;
 }
 
+//-------------------------------------------------------------------
+//Método: Caminho
+//Parametros:
+// Entrada:
+//     v1: vertice 1
+//     v2: vertice 2
+//	   custo_total: custo total entre o vertice 1 e o vertice 2
+// Retorno:
+//    caminho entre o vertice 1 e o vertice 2
+//-------------------------------------------------------------------
 list<int> *Grafo::Caminho(int v1, int v2, double &custo_total) {
 	return NULL;
 }
 
+//-------------------------------------------------------------------
+//Método: VerticesIsolados
+//Parametros:
+// Entrada:
+//
+// Retorno:
+//    Lista de vertices isolados
+//-------------------------------------------------------------------
 list<int> *Grafo::VerticesIsolados() {
 	return NULL;
 }
 
+//-------------------------------------------------------------------
+//Método: Search
+//Parametros:
+// Entrada:
+//     v: vertice a procurar
+// Retorno:
+//    true/false, dependendo se encontra ou nao o vertice
+//-------------------------------------------------------------------
 bool Grafo::Search(int v) {
 	return false;
 }
 
+//-------------------------------------------------------------------
+//Método: RemoverVertice
+//Parametros:
+// Entrada:
+//	   v: vertice a procurar 
+// Retorno:
+//    true/false, dependendo se o vertice foi removido ou nao
+//-------------------------------------------------------------------
 bool Grafo::RemoverVertice(int v) {
 	return false;
 }
 
+//-------------------------------------------------------------------
+//Método: RemoverAresta
+//Parametros:
+// Entrada:
+//	   v1: vertice 1
+//	   v2: vertice 2
+// Retorno:
+//    true/false, dependendo se o aresta foi removida ou nao
+//-------------------------------------------------------------------
 bool Grafo::RemoverAresta(int v1, int v2) {
 	return false;
 }
 
+//-------------------------------------------------------------------
+//Método: EscreverXML
+//Parametros:
+// Entrada:
+//	   s: Nome do ficheiro que se quer gravar as informacoes do grafo
+// Retorno:
+//
+//-------------------------------------------------------------------
 void Grafo::EscreverXML(const string &s) {
 
 }
 
+//-------------------------------------------------------------------
+//Método: LerXML
+//Parametros:
+// Entrada:
+//	   s: Nome do ficheiro que se quer ler as informacoes do grafo
+// Retorno:
+//    true/false, dependendo se o ficheiro foi lido ou nao
+//-------------------------------------------------------------------
 bool Grafo::LerXML(const string &s) {
 	return false;
 }
 
+//-------------------------------------------------------------------
+//Método: DevolveVerticesTipo
+//Parametros:
+// Entrada:
+//	   tipo: tipo de vertice pretendido
+// Retorno:
+//    lista com os vertices do grafo do tipo pretendido
+//-------------------------------------------------------------------
 list<int> *Grafo::DevolveVerticesTipo(const string &tipo) {
 	return NULL;
 }
 
+//-------------------------------------------------------------------
+//Método: CaminhoMinimo
+//Parametros:
+// Entrada:
+//	   v1: vertice 1
+//	   v2: vertice 2
+//	   custo_total: custo total entre os dois vertices
+// Retorno:
+//    lista dos vertices com o caminho mais curto entre v1 e v2
+//-------------------------------------------------------------------
 list<int> *Grafo::CaminhoMinimo(int v1, int v2, double &custo_total) {
 	return NULL;
 }
 
+//-------------------------------------------------------------------
+//Método: CaminhoMaximo
+//Parametros:
+// Entrada:
+//	   v1: vertice 1
+//	   v2: vertice 2
+//	   custo_total: custo total entre os dois vertices
+// Retorno:
+//    lista dos vertices com o caminho maximo entre v1 e v2
+//-------------------------------------------------------------------
 list<int> *Grafo::CaminhoMaximo(int v1, int v2, double &custo_total) {
 	return NULL;
 }
 
+//-------------------------------------------------------------------
+//Método: PossivelCaminho
+//Parametros:
+// Entrada:
+//	   v1: vertice 1
+//	   v2: vertice 2
+//	   TipoFronteira: tipo de uma fronteira
+// Retorno:
+//    true/falso, dependendo se existe caminho entre v1 e v2 por fronteiras do TipoFronteira
+//-------------------------------------------------------------------
 bool Grafo::PossivelCaminho(int v1, int v2, int TipoFronteira) {
 	return false;
 }
 
+//-------------------------------------------------------------------
+//Método: FronteirasMenosVisitadas
+//Parametros:
+// Entrada:
+//	   Lv: Lista de viagens
+// Retorno:
+//
+//-------------------------------------------------------------------
 void Grafo::FronteirasMenosVisitadas(list<int> &Lv) {
 
 }
 
+//-------------------------------------------------------------------
+//Método: SitiosInacessiveisAPartirDe
+//Parametros:
+// Entrada:
+//	   v1: vertice 1
+//	   Lv: Lista de viagens
+// Retorno:
+//
+//-------------------------------------------------------------------
 void Grafo::SitiosInacessiveisAPartirDe(int V1, list<int> &Lv) {
 
 }
--- Projeto POO/Grafo.h
+++ Projeto POO/Grafo.h
@@ -4,45 +4,44 @@ private:
 
 public:
 	Grafo();
-	//Implemente o destrutor do Grafo;
+	// Implemente o destrutor do Grafo;
 	~Grafo();
-	//Carregar os dados de ficheiros (do grafo e de pessoas);
+	// Carregar os dados de ficheiros (do grafo e de pessoas);
 	bool Load(const string &fich_grafo, const string &fich_pessoas);
-	//Contar o número de nós/fronteiras do grafo;
+	// Contar o número de nós/fronteiras do grafo;
 	int ContarNos();
-	//Contar o número de arestas/arcos do grafo;
+	// Contar o número de arestas/arcos do grafo;
 	int ContarArcos();
-	//Determinar toda a memória ocupada;
+	// Determinar toda a memória ocupada;
 	int Memoria();
-	//Determinar qual o nó/fronteira que tem mais arcos/aresta, se existirem vários devolve uma lista deles;
+	// Determinar qual o nó/fronteira que tem mais arcos/aresta, se existirem vários devolve uma lista deles;
 	list<int> *NoMaisArcos();
-	//Verificar se um nó é adjacente de outro nó do Grafo;
+	// Verificar se um nó é adjacente de outro nó do Grafo;
 	bool Adjacencia(int v1, int v2);
-	//Determinar um caminho(não interessa se é o mais rápido!, é qualquer um!) de um nó/fronteira para outro.... //devolvendo o custo total;
+	// Determinar um caminho(não interessa se é o mais rápido!, é qualquer um!) de um nó/fronteira para outro.... //devolvendo o custo total;
 	list<int> *Caminho(int v1, int v2, double &custo_total);
-	//Determinar quais os vértices que estão isolados (Um vértice é isolado se não existe nenhum caminho até ele!);
+	// Determinar quais os vértices que estão isolados (Um vértice é isolado se não existe nenhum caminho até ele!);
 	list<int> *VerticesIsolados();
-	//Verificar se um dado vértice existe
+	// Verificar se um dado vértice existe
 	bool Search(int v);
-	//Remover um dado vértice, também será necessário remover todas as arestas;
+	// Remover um dado vértice, também será necessário remover todas as arestas;
 	bool RemoverVertice(int v);
-	//Remover a aresta que liga 2 vértices;
+	// Remover a aresta que liga 2 vértices;
 	bool RemoverAresta(int v1, int v2);
-	//Gravar para ficheiro em formato XML todas as informações do Grafo;
+	// Gravar para ficheiro em formato XML todas as informações do Grafo;
 	void EscreverXML(const string &s);
-	//Ler de um ficheiro em formato XML todo o Grafo (antes de ler deve ser apagado tudo o que estiver no Grafo); Se leu corretamente devolve //true, senão devolve false;
+	// Ler de um ficheiro em formato XML todo o Grafo (antes de ler deve ser apagado tudo o que estiver no Grafo); Se leu corretamente devolve //true, senão devolve false;
 	bool LerXML(const string &s);
-	//Devolver uma lista de todos os vértices/fronteiras de um dado tipo;
+	// Devolver uma lista de todos os vértices/fronteiras de um dado tipo;
 	list<int> *DevolveVerticesTipo(const string &tipo);
-	//Determinar o caminho mínimo entre 2 nós, devolvendo o custo total;
+	// Determinar o caminho mínimo entre 2 nós, devolvendo o custo total;
 	list<int> *CaminhoMinimo(int v1, int v2, double &custo_total);
-	//Determinar o caminho máximo entre 2 nós (passando somente uma vez em cada vértice), devolvendo o custo total;
+	// Determinar o caminho máximo entre 2 nós (passando somente uma vez em cada vértice), devolvendo o custo total;
 	list<int> *CaminhoMaximo(int v1, int v2, double &custo_total);
-	//Será possível ir de uma fronteira(v1) a outra(v2), passando somente por fronteiras de um dado tipo?
+	// Será possível ir de uma fronteira(v1) a outra(v2), passando somente por fronteiras de um dado tipo?
 	bool PossivelCaminho(int v1, int v2, int TipoFronteira);
-	//Assumindo que todas as pessoas (do ficheiro) andaram em viagem, qual foi a fronteira com menos tráfego/visitas? (No caso de serem várias,// deve devolver uma lista com as fronteiras);
+	// Assumindo que todas as pessoas (do ficheiro) andaram em viagem, qual foi a fronteira com menos tráfego/visitas? (No caso de serem várias,// deve devolver uma lista com as fronteiras);
 	void FronteirasMenosVisitadas(list<int> &Lv);
-
 	//Dada uma fronteira (v1), determinar todas as fronteiras onde não é possível chegar apartir de v1. Deve devolver uma lista dessas fronteiras!;
 	void SitiosInacessiveisAPartirDe(int V1, list<int> &Lv);
 };
