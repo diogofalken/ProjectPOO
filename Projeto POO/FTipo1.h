cf381a5a2b2cb400852a3956b3d9d46b19b017ad
Diogo Falcão <diogofalken0@gmail.com>
2018-10-28 15:39:53 +0000

    Did some changes on FTipo1 and FTipo2

--- Projeto POO/FTipo1.h
+++ Projeto POO/FTipo1.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Fronteira.h"
 class FTipo1 : public Fronteira {
+private:
+	unsigned int preco_pago;
 
 public:
 	FTipo1();
--- Projeto POO/FTipo2.h
+++ Projeto POO/FTipo2.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Fronteira.h"
 class FTipo2 : public Fronteira {
+private:
+	unsigned int peso_transportado;
 
 public:
 	FTipo2();
